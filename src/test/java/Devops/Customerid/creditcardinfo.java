7ae864ed3957810e85bf064e9920f9e726915de5
sasikalakrish <kp.sasikrishna@gmail.com>
2022-04-25 09:55:20 -0400

    report

--- src/test/java/Devops/Customerid/creditcardinfo.java
+++ src/test/java/Devops/Customerid/creditcardinfo.java
@@ -3,7 +3,7 @@ package Devops.Customerid;
 public class creditcardinfo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 System.out.println("Creditcard details");
 	}
 
